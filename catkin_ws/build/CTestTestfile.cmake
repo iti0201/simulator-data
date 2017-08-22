de31eaaab52a8ad67b2b6ae5d844f641360b3c55
Rasmus.Iila <rasmus.iila@ttu.ee>
2017-08-22 14:35:40 +0100

    let me just try this

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/osboxes/simulator-data/catkin_ws/src
+# Build directory: /home/osboxes/simulator-data/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(lego_gazebo)
---
+++ catkin_ws/build/lego_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/osboxes/simulator-data/catkin_ws/src/lego_gazebo
+# Build directory: /home/osboxes/simulator-data/catkin_ws/build/lego_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
